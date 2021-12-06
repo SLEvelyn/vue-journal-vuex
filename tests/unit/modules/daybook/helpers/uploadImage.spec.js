6a2139e894f2bd47c32b4052fcc6d19dd55eb5b3
SLEvelyn <nicolcrack@gmail.com>
2021-12-06 10:26:05 -0500

    Test:units archivos subidos borrados

--- tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -1,11 +1,21 @@
+import cloudinary from 'cloudinary'
 import uploadImage from '@/modules/daybook/helpers/uploadImage'
 import axios from 'axios'
 
+
+
+cloudinary.config({
+    cloud_name: 'evelynsl',
+    api_key: '887451317651727',
+    api_secret: 'Bx9P64AJlp3Nn-0WB49uvdxo-tU'
+})
+
+
 describe('Prueba en el uploadImage', () => {
 
-    test('debe de cargar un archivo y retornar el url', async() => {
+    test('debe de cargar un archivo y retornar el url', async(done) => {
 
-        const {data} = await axios.get('https://res.cloudinary.com/evelynsl/image/upload/v1636985061/burp7wc8mg0nnxqdp6hj.jpg', {
+        const {data} = await axios.get('https://res.cloudinary.com/evelynsl/image/upload/v1636725878/st8qqjnsbcwt7dm9nyrz.jpg', {
             responseType: 'arraybuffer'
         }) 
 
@@ -14,6 +24,13 @@ describe('Prueba en el uploadImage', () => {
         const url = await uploadImage (file)
 
         expect(typeof url).toBe('string')
+
+        //Tomar el ID
+        const segments = url.split('/')
+        const imageId = segments[segments.length -1].replace('.jpg', '')
+        cloudinary.v2.api.delete_resources(imageId, {}, () => {
+            done()
+        })
     })
 
 
