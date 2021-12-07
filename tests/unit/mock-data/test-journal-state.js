b75cc9b47a75f4b6878188d6bc5912e0cada0db0
SLEvelyn <nicolcrack@gmail.com>
2021-12-07 10:29:14 -0500

    Test:units Initial State and mutations setEntries

---
+++ tests/unit/mock-data/test-journal-state.js
@@ -0,0 +1,18 @@
+
+export const journalState = {
+    isLoading: false,
+    entries: [
+        {
+            id: '-MoEI177zsJTzGQhcEGO',
+            date : 1636639167856,
+            // picture : "https://res.cloudinary.com/evelynsl/image/upload/v1636985026/eoqtft4usuubgufjamcn.jpg",
+            text : "Hola mundo desde mock data"
+        }, 
+        {
+            id: '-MoYuPkrHO0Vhd7SNvXM',
+            date : 1636985035886,
+            picture : "https://res.cloudinary.com/evelynsl/image/upload/v1636985061/burp7wc8mg0nnxqdp6hj.jpg",
+            text : "Hola mundo "
+        }
+    ]
+}
---
+++ tests/unit/modules/store/journal/journal-module.spec.js
@@ -0,0 +1,39 @@
+import {createStore} from 'vuex'
+import journal from '@/modules/daybook/store/journal'
+import {journalState} from '../../../mock-data/test-journal-state'  
+
+const createVuexStore = (initialState) => 
+    createStore ({
+        modules: {
+            journal: {
+                ...journal,
+                state: {...initialState}
+            }
+        }
+    })
+
+describe('Vuex - Pruebas en el Journal Module', () => {
+
+    //Basicas
+    test('Este es el estado inicila, debe de tener este state', () => {
+
+        const store = createVuexStore(journalState)
+        const {isLoading, entries} = store.state.journal 
+
+        expect(isLoading).toBeFalsy(),
+        expect(entries).toEqual(journalState.entries)
+
+    })
+
+    //Mutations
+    test('mutation: setEntries', () => {
+
+        const store = createVuexStore({isLoading: true, entries: []})
+
+        store.commit('journal/setEntries', journalState.entries)
+        expect(store.state.journal.entries.length).toBe(2)
+
+        expect(store.state.journal.isLoading).toBeFalsy()
+
+    })
+})
