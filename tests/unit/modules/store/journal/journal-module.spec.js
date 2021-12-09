be59c5a9ed226a361c2dbf92a9fa698f8a0fce31
SLEvelyn <nicolcrack@gmail.com>
2021-12-09 09:59:58 -0500

    Test:units mutations: updatedEntry

--- tests/unit/modules/store/journal/journal-module.spec.js
+++ tests/unit/modules/store/journal/journal-module.spec.js
@@ -2,6 +2,7 @@ import {createStore} from 'vuex'
 import journal from '@/modules/daybook/store/journal'
 import {journalState} from '../../../mock-data/test-journal-state'  
 
+
 const createVuexStore = (initialState) => 
     createStore ({
         modules: {
@@ -34,6 +35,31 @@ describe('Vuex - Pruebas en el Journal Module', () => {
         expect(store.state.journal.entries.length).toBe(2)
 
         expect(store.state.journal.isLoading).toBeFalsy()
+
+    })
+
+    test('Mutation: updateEntry', () => {
+        //create store con entries
+        const store = createVuexStore(journalState)
+
+        //updatedEntry
+        const updatedEntry = {
+            id: '-MoEI177zsJTzGQhcEGO',
+            date : 1636639167856,
+            text : "Hola mundo desde pruebas"
+        }
+
+        //commit de la mutacion
+        store.commit('journal/updateEntry', updatedEntry)
+
+        //Expects
+        const storeEntries = store.state.journal.entries
+
+        expect(storeEntries.length).toBe(2)
+
+        expect(
+            storeEntries.find(e => e.id === updatedEntry.id)
+        ).toEqual(updatedEntry)
 
     })
 })
